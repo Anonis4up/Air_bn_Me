aecc723e43af665bff781850a12c38dd99419290
fred anonis <anonis@hotmail.fr>
2025-02-13 19:05:59 +0100

    des wagons

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
+  get 'reservations/create'
   devise_for :users
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :bikes
+  resources :bikes, only: [:index, :show, :new, :create] do
+    resources :reservations, only: [:create]
+  end
+  resources :reservations, only: [:index, :destroy]
+
+  root to: "bikes#index"
   # Defines the root path route ("/")
   # root "posts#index"
 end
