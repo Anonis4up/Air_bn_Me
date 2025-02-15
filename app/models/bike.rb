c7644fd950ad092e95d6bcf159b5ab2c30d9b6bf
joey collier <joey.collier.pro@gmail.com>
2025-02-15 16:26:47 +0100

    rattachement de la map/bike select ok

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -3,12 +3,12 @@ class Bike < ApplicationRecord
   has_many :reservations
   has_one_attached :photo
 
-  def address
-    user&.address
-  end
-
   geocoded_by :address
   after_validation :geocode
 
   validates :brand, :price_per_day, :location, presence: true
+
+  def address
+    user&.address
+  end
 end
