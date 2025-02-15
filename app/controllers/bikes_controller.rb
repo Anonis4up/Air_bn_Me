96d756d4177e3ee90cf6a6bfcefa9e29052d7c6a
fred anonis <anonis@hotmail.fr>
2025-02-15 14:55:02 +0100

    modifJOey et moi

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -2,7 +2,13 @@ class BikesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show]
 
 def index
-@bikes = Bike.all
+  @bikes = Bike.all
+  @markers = @bikes.geocoded.map do |bike|
+    {
+      lat: bike.latitude,
+      lng: bike.longitude
+    }
+  end
 end
 
 def show
