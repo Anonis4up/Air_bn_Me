cf9526f8fff39e37f360bf8ea446b958ed1c2de8
Mara <bratosinralucamara@yahoo.com>
2025-02-21 21:16:42 +0100

    reviews and destroy

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,7 +1,7 @@
 class Bike < ApplicationRecord
   belongs_to :user
-  has_many :reservations
-  has_many :bookmarks
+  has_many :reservations, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   has_one_attached :photo
 
